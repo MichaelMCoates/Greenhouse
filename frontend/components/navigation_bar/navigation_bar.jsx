046c0b72ce100b2a98ee249c06c0bec5066528aa
Michael Coates <MichaelMCoates@gmail.com>
2017-04-20 00:53:31 -0400

    Style Nav Bar

--- frontend/components/navigation_bar/navigation_bar.jsx
+++ frontend/components/navigation_bar/navigation_bar.jsx
@@ -5,8 +5,18 @@ import UserBarContainer from '../user_bar/user_bar_container';
 const NavigationBar = () => {
   return (
     <nav className="navigation-bar">
-      <div>"Logo"</div>
-      <UserBarContainer />
+      <div className="left">
+        <div>"Logo"</div>
+        <button >Explore</button>
+        <button >How It Works</button>
+        <button >Equity Offerings</button>
+          <input className="search-bar" type="text" placeholder="Search..."></input>
+      </div>
+
+      <div className="right">
+        <input className="pink-button-nav" type="submit" value="START A CAMPAIGN" />
+        <UserBarContainer />
+      </div>
     </nav>
   );
 };
