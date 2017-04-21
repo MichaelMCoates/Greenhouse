982302ef7dcaf4cc86d04f9813f5e345ec730fbf
Michael Coates <MichaelMCoates@gmail.com>
2017-04-21 11:42:32 -0400

    Format campaign summary and start on campaigner details

--- frontend/components/campaign/campaign_summary/campaign_summary.jsx
+++ frontend/components/campaign/campaign_summary/campaign_summary.jsx
@@ -12,6 +12,8 @@ class CampaignSummary extends React.Component {
       <div className="campaign-summary campaign-show-right">
         <h1 className="campaign-title">{this.props.title}</h1>
         <h2 className="campaign-tagline">{this.props.tagline}</h2>
+
+        <br/>
         // CampaignerDetails component, uses user_id. Uses Modal for AboutBox
         // FundingBar component, uses goal_amt, current_amt, created_at, duration
         // BackItBar component, uses state. Has SocialBar and DonationBar
