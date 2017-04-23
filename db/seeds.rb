884a634257739d48f088ca25531d8f7e0a6205b2
Michael Coates <MichaelMCoates@gmail.com>
2017-04-23 06:33:04 -0400

    Change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,27 @@ bill_nye = User.create({
   country: "United States",
 })
 
+hexagon_user = User.create({
+  email: "HEXAGON@gmail.com",
+  password: "HEXAGON",
+  first_name: "HEXAGON",
+  last_name: "by Smart Bikes",
+  city: "Los Angeles",
+  country: "United States",
+})
+
 
 tv_show = Campaign.create({
-  user_id: bill_nye.id,
-  goal_amt: 10000,
-  current_amt: 7486,
-  title: "Bill Nye The Science Guy",
-  tagline: "It's my show!",
+  user_id: hexagon_user.id,
+  goal_amt: 30000,
+  current_amt: 54090,
+  title: "HEXAGON - Camera, Signals, & Sensors for Cyclists",
+  tagline: "With a full HD camera, power bank, crash sensor, & more HEXAGON is the ultimate safety device.",
   city: "Hollywood",
   country: "United States",
-  duration: 60,
-  overview: "This is some overview text",
-  campaign_story: "This is a huge campaign story",
+  duration: 6,
+  overview: "With a revolutionary Full HD (1080p/720p)  rear-view camera, smart sensors, and a complementary app, HEXAGON is the ultimate safety device for cyclists. Our streaming camera, power bank, activity tracker, odometer, and other smart safety features will be sure to make your ride safe and connected. Let HEXAGON change the way you ride.",
+  campaign_story: "HEXAGON makes any bike a safer and smarter by seamlessly connecting with your smartphone. View the traffic behind you, live stream your ride, & signal to cars all through smartphone integration. With our rear-facing camera, you can view traffic & stream your ride.HEXAGON has Automatic Stop, Turn, and Motion signals.iOS & Android app with activity tracker, navigation system, and more.Award-winning cyclist, Vittorio Brumotti, loves HEXAGON. Check out the video below to hear Vittorio's stamp of approval!HEXAGON is equipped with a rear-facing HD camera, Automatic Stop, Turn, and Motion signals. To navigate the turn signals, HEXAGON comes with a wireless remote that securely attaches to your handle bars. HEXAGON's social and recording features can be managed through the HEXAGON app.HEXAGON comes with two 18650 batteries and can easily be recharged with the complimentary Micro-USB cable. HEXAGON Light has all the great features of HEXAGON, minus the turn signals and the Wireless Remote Control. This slimmed down model has the same rear-facing HD camera, Automatic Stop, and Motion signals. All of HEXAGON Light's features and settings can be managed through the HEXAGON app, so there's no need for the remote!  HEXAGON Light comes with two 18650 batteries and can easily be recharged with the complimentary Micro-USB cable. ",
 })
 
 cont = Contribution.create({
--- frontend/components/campaign/media_box/media_box.jsx
+++ frontend/components/campaign/media_box/media_box.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MediaBox = (params) => (
-  <div className="media-box campaign-show-left"><img src="http://lorempixel.com/400/265/" /></div>
+  <div className="media-box campaign-show-left"><img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_413,w_620/v1489425148/m5xa9uah5upbxobwtkli.jpg" /></div>
 );
 
 export default MediaBox;
