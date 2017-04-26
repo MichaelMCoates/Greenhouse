81894eafa8571095317cb8c71d76dd799c5fd980
Michael Coates <MichaelMCoates@gmail.com>
2017-04-26 16:01:57 -0400

    Style Story Page

--- frontend/components/campaign_create/create_body/basics_editor.jsx
+++ frontend/components/campaign_create/create_body/basics_editor.jsx
@@ -5,60 +5,60 @@ class BasicsEditor extends React.Component {
   render() {
 
     return (
-      <div className="basics-editor">
-        <div className="basics-editor-header">Basics</div>
-        <div className="basics-editor-description">Make a good first impression: introduce your campaign objectives and entice people to learn more. This basic information will represent your campaign on your campaign page, on your campaign card, and in searches.</div>
+      <div className="editor">
+        <div className="editor-header">Basics</div>
+        <div className="editor-description">Make a good first impression: introduce your campaign objectives and entice people to learn more. This basic information will represent your campaign on your campaign page, on your campaign card, and in searches.</div>
 
 
-        <div className="basics-editor-input-div">
-          <div className="basics-editor-input-title">Campaign Title</div>
-          <div className="basics-editor-input-description">What is the title of your campaign?</div>
+        <div className="editor-input-div">
+          <div className="editor-input-title">Campaign Title</div>
+          <div className="editor-input-description">What is the title of your campaign?</div>
           <input type="text"
             value={this.props.state.title}
             onChange={this.props.update('title')}
-            className="basics-editor-input"
+            className="editor-input"
             />
         </div>
 
 
-        <div className="basics-editor-input-div">
-          <div className="basics-editor-input-title">Campaign Tagline</div>
-          <div className="basics-editor-input-description">Provide a short description that best describes your campaign to your audience.</div>
+        <div className="editor-input-div">
+          <div className="editor-input-title">Campaign Tagline</div>
+          <div className="editor-input-description">Provide a short description that best describes your campaign to your audience.</div>
           <textarea type="text"
             value={this.props.state.tagline}
             onChange={this.props.update('tagline')}
-            className="basics-editor-textarea"
+            className="editor-textarea"
             />
         </div>
 
 
-        <div className="basics-editor-input-div">
-          <div className="basics-editor-input-title">Location</div>
-          <div className="basics-editor-input-description">Choose the location where you are running the campaign. This location will be visible on your campaign page for your audience to see.</div>
+        <div className="editor-input-div">
+          <div className="editor-input-title">Location</div>
+          <div className="editor-input-description">Choose the location where you are running the campaign. This location will be visible on your campaign page for your audience to see.</div>
           <div className="location">
             <input type="text"
               value={this.props.state.city}
               onChange={this.props.update('city')}
-              className="basics-editor-input"
+              className="editor-input"
               placeholder="City"
               />
             <input type="text"
               value={this.props.state.country}
               onChange={this.props.update('country')}
-              className="basics-editor-input"
+              className="editor-input"
               placeholder="Country"
               />
           </div>
         </div>
 
 
-        <div className="basics-editor-input-div">
-          <div className="basics-editor-input-title">Campaign Duration</div>
-          <div className="basics-editor-input-description">How many days will you be running your campaign for? You can run a campaign for any number of days, with a 60 day duration maximum.</div>
+        <div className="editor-input-div">
+          <div className="editor-input-title">Campaign Duration</div>
+          <div className="editor-input-description">How many days will you be running your campaign for? You can run a campaign for any number of days, with a 60 day duration maximum.</div>
           <input type="number"
             value={this.props.state.duration}
             onChange={this.props.update('duration')}
-            className="basics-editor-input duration-input"
+            className="editor-input duration-input"
             />
         </div>
 
--- frontend/components/campaign_create/create_body/story_editor.jsx
+++ frontend/components/campaign_create/create_body/story_editor.jsx
@@ -5,31 +5,31 @@ class StoryEditor extends React.Component {
   render() {
 
     return (
-      <div className="story-editor">
-        <div className="story-editor-header">Campaign Overview</div>
-        <div className="story-editor-description">Introduce yourself, your background, your campaign and why it’s important to you. Express the magnitude of what contributors will help you achieve.</div>
+      <div className="editor">
+        <div className="editor-header">Campaign Overview</div>
+        <div className="editor-description">Introduce yourself, your background, your campaign and why it’s important to you. Express the magnitude of what contributors will help you achieve.</div>
 
 
-        <div className="story-editor-input-div">
-          <div className="story-editor-input-title">Campaign Overview</div>
-          <div className="story-editor-input-description">Lead with a compelling statement that describes your campaign and why it’s important to you, highlight key campaign features, and remember - keep it short!</div>
+        <div className="editor-input-div">
+          <div className="editor-input-title">Campaign Overview</div>
+          <div className="editor-input-description">Lead with a compelling statement that describes your campaign and why it’s important to you, highlight key campaign features, and remember - keep it short!</div>
           <textarea
             value={this.props.state.overview}
             onChange={this.props.update('overview')}
-            className="story-editor-textarea"
+            className="editor-textarea-overview"
             />
         </div>
 
         <hr/>
 
-        <div className="story-editor-header">Campaign Pitch</div>
-        <div className="story-editor-description">Tell potential contributors more about your campaign. Provide details that will motivate people to contribute. A good pitch is compelling, informative, and easy to digest.</div>
+        <div className="editor-header-pitch">Campaign Pitch</div>
+        <div className="editor-description-pitch">Tell potential contributors more about your campaign. Provide details that will motivate people to contribute. A good pitch is compelling, informative, and easy to digest.</div>
 
-        <div className="story-editor-input-div">
+        <div className="editor-input-div-pitch">
           <textarea
             value={this.props.state.campaign_story}
             onChange={this.props.update('campaign_story')}
-            className="story-editor-textarea"
+            className="editor-textarea-pitch"
             />
         </div>
 
