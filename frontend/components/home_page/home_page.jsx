2f51466cc6ef30fdf3cb90c108b1bebe1aeeaa9c
Michael Coates <MichaelMCoates@gmail.com>
2017-04-27 09:19:25 -0400

    start home page

---
+++ frontend/components/home_page/home_page.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var Slider = require('react-slick');
+
+class SimpleSlider extends React.Component {
+  render: function () {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+    );
+  }
+}
