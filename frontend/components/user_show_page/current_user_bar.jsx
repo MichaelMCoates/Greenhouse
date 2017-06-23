ad298c8e9ba75a8612321c0cf23ade26307f0903
Michael Coates <MichaelMCoates@gmail.com>
2017-06-22 21:44:35 -0400

    enableBackground

--- frontend/components/user_show_page/current_user_bar.jsx
+++ frontend/components/user_show_page/current_user_bar.jsx
@@ -19,7 +19,7 @@ class CurrentUserBar extends React.Component {
           </Link>
           <Link to={'/users/' + this.props.userId + '/edit'}
             className={this.props.page == "edit-page" ? "cub-button cub-button-selected": "cub-button"}>
-            <svg enable-background="new 0 0 64 64" height="64px" id="Layer_1" version="1.1" viewBox="0 0 64 64" width="64px" xmlns="http://www.w3.org/2000/svg">
+            <svg enableBackground="new 0 0 64 64" height="64px" id="Layer_1" version="1.1" viewBox="0 0 64 64" width="64px" xmlns="http://www.w3.org/2000/svg">
               <g>
               <path d="M55.736,13.636l-4.368-4.362c-0.451-0.451-1.044-0.677-1.636-0.677c-0.592,0-1.184,0.225-1.635,0.676l-3.494,3.484   l7.639,7.626l3.494-3.483C56.639,15.998,56.639,14.535,55.736,13.636z"/>
               <polygon points="21.922,35.396 29.562,43.023 50.607,22.017 42.967,14.39  "/>
