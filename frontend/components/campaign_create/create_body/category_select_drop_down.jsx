2fb71eacd8c22def7fc31bb1de44c33a8c86f129
Michael Coates <MichaelMCoates@gmail.com>
2017-07-01 19:22:06 -0400

    Functional dropdown

--- frontend/components/campaign_create/create_body/category_select_drop_down.jsx
+++ frontend/components/campaign_create/create_body/category_select_drop_down.jsx
@@ -4,10 +4,45 @@ import React from 'react';
 class CategorySelectDropdown extends React.Component {
 
   render() {
+    const techCategories = [
+      "Home",
+      "Phones & Accessories",
+      "Travel & Outdoors",
+      "Health & Fitness",
+      "Fashion & Wearables",
+    ]
+
+    const creativeCategories = [
+      "Film",
+      "Tabletop Games",
+      "Music",
+      "Art",
+      "Food & Beverages",
+    ]
+
+    const techDivs = techCategories.map ( (category, idx) => {
+      return (
+        <div key={idx} className="category-wrapper" onClick={() => this.props.setState({category})}>
+          {category}
+        </div>
+      )
+    })
+
+    const creativeDivs = creativeCategories.map ( (category, idx) => {
+      return (
+        <div key={idx} className="category-wrapper" onClick={() => this.props.setState({category})}>
+          {category}
+        </div>
+      )
+    })
+
+
+
 
     return (
       <div className="category-select-dropdown">
-        fawefaew
+        {techDivs}
+        {creativeDivs}
       </div>
     );
   }
