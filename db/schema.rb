533597e1874450aacebdcd53ba02c577f46730c1
Michael Coates <MichaelMCoates@gmail.com>
2017-07-02 18:50:29 -0400

    Add pg_tgrm for trigram searching

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628014555) do
+ActiveRecord::Schema.define(version: 20170702220837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "campaigns", force: :cascade do |t|
     t.integer  "user_id",                   null: false
