24f042ecfebb721eec08f6a32346464e86da91ef
Michael Coates <MichaelMCoates@gmail.com>
2017-07-02 15:29:52 -0400

    Finish styling Category Boxes

--- frontend/components/home_page/category_boxes.jsx
+++ frontend/components/home_page/category_boxes.jsx
@@ -19,31 +19,26 @@ class CategoryBoxes extends React.Component {
 
             <div className="category-box home">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
               <div className="cat-box-title">Home</div>
             </div>
 
             <div className="category-box phones">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Phones</div>
+              <div className="cat-box-title">Phones & Accessories</div>
             </div>
 
             <div className="category-box travel">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Travel</div>
+              <div className="cat-box-title">Travel & Outdoors</div>
             </div>
 
             <div className="category-box film">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
               <div className="cat-box-title">Film</div>
             </div>
 
             <div className="category-box art">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
               <div className="cat-box-title">Art</div>
             </div>
           </div>
@@ -51,32 +46,27 @@ class CategoryBoxes extends React.Component {
           <div className="box-container-row">
             <div className="category-box health">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Health</div>
+              <div className="cat-box-title">Health & Fitness</div>
             </div>
 
             <div className="category-box fashion">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Fashion</div>
+              <div className="cat-box-title">Fashion & Wearables</div>
             </div>
 
             <div className="category-box tabletop">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Tabletop</div>
+              <div className="cat-box-title">Tabletop Games</div>
             </div>
 
             <div className="category-box music">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
               <div className="cat-box-title">Music</div>
             </div>
 
             <div className="category-box food">
               <div className="cat-box-bg"/>
-              <div className="cat-box-icon"></div>
-              <div className="cat-box-title">Food</div>
+              <div className="cat-box-title">Food & Beverages</div>
             </div>
           </div>
         </div>
